1ae1d803155a9947ab9d373145c8b2c6e78d801b
Noran612 <noranomar784@gmail.com>
2025-03-20 17:49:12 +0200

    added task 2

--- lab2.py
+++ lab2.py
@@ -1,51 +1,59 @@
-from time import sleep
-
+#import typer
+import numpy as np
 from lab_ur5.motion_planning.motion_planner import MotionPlanner
 from lab_ur5.motion_planning.geometry_and_transforms import GeometryAndTransforms
 from lab_ur5.manipulation.manipulation_controller import ManipulationController
 from lab_ur5.robot_inteface.robots_metadata import ur5e_1, ur5e_2
+workspace_x_lim = [-1.0, 1]
+workspace_y_lim = [-1.0, 1]
 
-def meet_at_middle(robot1, robot2, axis='x', mid_point=[-0.4, -0.6], offset=0.08):
-    r1_point = mid_point.copy()
-    r2_point = mid_point.copy()
 
-    if axis == "x":
-        r1_point[0] += offset
-        r2_point[0] -= offset
-    elif axis == "y":
-        r1_point[1] += offset
-        r2_point[1] -= offset
-
-    robot1.plan_and_move_to_xyzrz(r1_point[0], r1_point[1], 0.2, 0)
-    robot2.plan_and_move_to_xyzrz(r2_point[0], r2_point[1], 0.2, 0)
-
-def relay_race(start_position, end_position):
+def relay_race(start_position, handover_position):
     motion_planner = MotionPlanner()
     gt = GeometryAndTransforms.from_motion_planner(motion_planner)
-    robot1 = ManipulationController(ur5e_1["ip"], ur5e_1["name"], motion_planner, gt)
-    robot2 = ManipulationController(ur5e_2["ip"], ur5e_2["name"], motion_planner, gt)
 
-    robot1.speed = 0.3
-    robot1.acceleration = 0.3
-    robot2.speed = 0.3
-    robot2.acceleration = 0.3
+    r1_controller = ManipulationController(ur5e_1["ip"],ur5e_1["name"],motion_planner,gt)
+    r1_controller.speed = 2
+    r1_controller.acceleration = 0.3
 
-    robot1.move_home()
-    robot2.move_home()
+    r2_controller = ManipulationController(ur5e_2["ip"],ur5e_2["name"],motion_planner,gt)
+    r2_controller.speed = 2
+    r2_controller.acceleration = 0.3
 
-    robot1.pick_up(start_position[0], start_position[1], start_position[2])
+    if not(workspace_x_lim[0] <= start_position[0] <= workspace_x_lim[1] and
+        workspace_y_lim[0] <= start_position[1] <= workspace_y_lim[1]):
+        raise ValueError("target_position must be within workspace_x_lim")
 
-    meet_at_middle(robot1, robot2, axis='x', mid_point=[-0.4, -0.6])
+    if not(workspace_x_lim[0] <= handover_position[0] <= workspace_x_lim[1] and
+        workspace_y_lim[0] <= handover_position[1] <= workspace_y_lim[1]):
+        raise ValueError("target_position must be within workspace_x_lim")
 
-    robot1.grasp_object()
-    robot2.release_object()
+    r1_controller.move_home()
+    r2_controller.move_home()
 
-    robot2.put_down(end_position[0], end_position[1], end_position[2])
+    x_start, y_start, z_start = start_position
+    r1_controller.pick_up(x_start, y_start, 0)
+    r1_joints = [-2.183211628590719, -2.7464448414244593, -0.8477506041526794, -1.148409680729248, -1.5770967642413538, 2.22922682762146]
+    r1_controller.moveJ(r1_joints)
 
-    sleep(5)
+    r2_joints =[1.0078383684158325, -0.3228061956218262, -0.9654921293258667, -0.3881130975535889, -1.599175755177633, 2.573286771774292]
+    r2_controller.moveJ(r2_joints)
+
+    tcp1_xyz = r1_controller.getActualTCPPose()
+    tcp1_joints = r1_controller.getActualQ()
+
+    print("r1 xyz")
+    print(tcp1_xyz)
+    print("r1 joints")
+    print(tcp1_joints)
 
 
-if __name__ == "__main__":
-    initial_position = [0.369, 0.0, 0.03]
-    end_position = [-0.72, -0.5, 0.03]
-    relay_race(initial_position, end_position)
+    tcp2_xyz = r2_controller.getActualTCPPose()
+    tcp2_joints = r2_controller.getActualQ()
+
+    print("r2 xyz:")
+    print(tcp2_xyz)
+    print("r2 joints:")
+    print(tcp2_joints)
+
+relay_race([0.3, 0, 0.03], [-0.5, -0.45, 0.15])
